d5a4bf6ddc0c245853d9f1e358386b6731368a51
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2022-09-05 19:50:00 +0200

    comment tweaks

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pgcbots-exts-template
-A template repository for creating `discord.ext.commands` extension modules using discord.py. 
+A template repository for creating `discord.ext.commands` extension modules using discord.py.
--- extension/__init__.py
+++ extension/__init__.py
@@ -1,17 +1,14 @@
 """This file is required to initialize the extension codebase as a package module.
 """
 
-import discord
-from discord.ext import commands
-
-
 __title__ = "TemplateExtension"  # TODO: Rename TemplateBot according to your extension.
 __author__ = "Extension-Author"  # TODO: Rename Bot-Author according to your extension.
 __license__ = "MIT"  # TODO: Change license according to your extension.
 __copyright__ = "Copyright 2022-present Bot-Author"  # TODO: Change copyright according to your extension.
-# a bot application instance at a time
 __version__ = "0.1.0"
-__uuid__ = "00000000-0000-0000-0000-000000000000"  # must be unique among all extensions loaded in
+__uuid__ = "00000000-0000-0000-0000-000000000000"
+# __uuid__ must be unique among all extensions loaded in
+# a bot application instance at a time, can be used as keys in a database
 
 
 def setup(bot):
